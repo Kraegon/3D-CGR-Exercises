9de6ac83f95fbac72007f280f28bc758132ff5db
Guus Beckett <ghbeckett@gmail.com>
2014-04-17 12:03:32 +0200

    Added initialization code.

---
+++ Week_1/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  OpdrachtenWeek1
+//
+//  Created by Guus Beckett and Julian West on 17-04-14.
+//  Copyright (c) 2014 reupload.nl. All rights reserved.
+//
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(800, 600);
+    glutInit(&argc, argv);
+    glutCreateWindow("Hello Julian");
+    glutMainLoop();
+}
+
+
+

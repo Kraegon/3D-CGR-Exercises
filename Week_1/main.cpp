7ce19541fd4d603d7b065b596c3c6a80578fad20
Julian G. West <Julio34@gmail.com>
2014-04-17 13:58:59 +0200

    Two quads spinning - Week 1

--- Week_1/main.cpp
+++ Week_1/main.cpp
@@ -13,11 +13,32 @@
 #include <iostream>
 #include <stdio.h>
 
+float rotation = 0.0f;
+float eyepos = 0.0f;
+
 void onDisplay(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
-	printf("test\n");
+	gluLookAt(0,0,10, 	//Eye
+			  0,0,0, 	//Center
+			  0,1,0);	//Up 
+
+	glRotatef(rotation,0,1,0);
+
+	glBegin(GL_QUADS);
+		glVertex3f(0,0,0);
+		glVertex3f(0,1,0);
+		glVertex3f(1,1,0);
+		glVertex3f(1,0,0);
+
+		glVertex3f(0,0,1);
+		glVertex3f(0,1,1);
+		glVertex3f(1,1,1);
+		glVertex3f(1,0,1);
+
+	glEnd();
+
 	glutSwapBuffers();
 }
 
@@ -28,6 +49,10 @@ void Display(void)
 
 void Reshape(GLint width, GLint height)
 {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(90,width/height,1,100);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void InitGraphics(void)
@@ -44,16 +69,24 @@ void MouseMotion(int x, int y)
 
 void IdleFunc(void)
 {
+	rotation++;
     glutPostRedisplay();
 }
 
 void Keyboard(unsigned char key, int x, int y)
 {
+	printf("%d\n", key);
 	switch (key)
 	{
         case 27:             // ESCAPE key
             exit (0);
             break;
+       	case 97:
+        	eyepos+=0.1f;
+       		break;
+       	case 100:
+        	eyepos-=0.1f;
+       		break;
 	}
 }
 
@@ -63,8 +96,13 @@ int main(int argc, char * argv[])
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutInitWindowSize(800, 600);
     glutInit(&argc, argv);
-    glutCreateWindow("Hello Guus & Julian");
- 	glutDisplayFunc(onDisplay);
+    glutCreateWindow("Hello Guus & Julian");	
+	glutDisplayFunc (onDisplay);
+	glutReshapeFunc (Reshape);
+	glutKeyboardFunc (Keyboard);
+	glutMouseFunc (MouseButton);
+	glutMotionFunc (MouseMotion);
+	glutIdleFunc (IdleFunc);
     glutMainLoop();
 }
 

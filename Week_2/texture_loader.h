26bd125e01fe7241bd37dc72a3c54b9dfbbd8139
Guus Beckett <ghbeckett@gmail.com>
2014-04-26 12:25:26 +0200

    Added some functionality to the class texture_loader

--- Week_2/texture_loader.h
+++ Week_2/texture_loader.h
@@ -1,12 +1,15 @@
+#include <string>
 class texture_loader
 {
 private:
 	int a;
+    std::string texturePath;
 
 public:
-	texture_loader(int a) : a(a){
-		//Do texture things
-	}
+	texture_loader(int a);
+    texture_loader(std::string fileName);
+    
 
 	int texMethodExample(int a, int b);
+    
 };
